137bc92355c5fc000aba34d983a601ca1c1d7e41
WangBin <wbsecg1@gmail.com>
2011-09-18 18:00:28 +0800

    Fixed crash for windows

--- src/qdevicewatcher_win.cpp
+++ src/qdevicewatcher_win.cpp
@@ -131,6 +131,7 @@ static inline QString className()
 
 static inline HWND dw_create_internal_window(const void* userData)
 {
+	QString className = ::className();
 	HINSTANCE hi = qWinAppInst();
 
 	WNDCLASS wc;
@@ -143,7 +144,7 @@ static inline HWND dw_create_internal_window(const void* userData)
 	wc.hCursor = 0;
 	wc.hbrBackground = 0;
 	wc.lpszMenuName = NULL;
-	wc.lpszClassName = reinterpret_cast<const wchar_t *>(className().utf16());
+	wc.lpszClassName = reinterpret_cast<const wchar_t *>(className.utf16());
 	RegisterClass(&wc);
 
 	HWND hwnd = CreateWindow(wc.lpszClassName,       // classname
