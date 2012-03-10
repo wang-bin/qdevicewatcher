d8785a2e3bd9729c19e741aee9b0ab5aa6fafae0
WangBin <wbsecg1@gmail.com>
2012-03-10 16:01:22 +0800

    Fix win32 events just post the last 1.
    Try to use RegisterDeviceNotification(), but not complete yet.

--- src/qdevicewatcher_win32.cpp
+++ src/qdevicewatcher_win32.cpp
@@ -35,12 +35,26 @@
 #  define DBT_CUSTOMEVENT 0x8006
 #endif
 
+#define CONFIG_NOTIFICATION 0
+
+/*
+ *http://www.codeproject.com/Articles/14500/Detecting-Hardware-Insertion-and-or-Removal
+ *http://blog.csdn.net/sxbluebird/article/details/2445145
+*/
+#if CONFIG_NOTIFICATION
+static HDEVNOTIFY hDevNotify = 0;
+#define HID_CLASSGUID {0x4d1e55b2, 0xf16f, 0x11cf,{ 0x88, 0xcb, 0x00, 0x11, 0x11, 0x00, 0x00, 0x30}}
+static const GUID WceusbshGUID = { 0x25dbce51, 0x6c8f, 0x4a72, 0x8a,0x6d,0xb5,0x4c,0x2b,0x4f,0xc8,0x35 };
+static const GUID GUID_DEVINTERFACE_USBSTOR = { 0xA5DCBF10L, 0x6530, 0x11D2, { 0x90, 0x1F, 0x00, 0xC0, 0x4F, 0xB9, 0x51, 0xED } };
+static const GUID InterfaceClassGuid = GUID_DEVINTERFACE_USBSTOR;//(GUID)HID_CLASSGUID; //GUID_DEVINTERFACE_USBSTOR
+#endif //CONFIG_NOTIFICATION
+
 Q_CORE_EXPORT HINSTANCE qWinAppInst();
 
-static inline QStringList drivesFromMask(quint32 driveBits)
+static inline QStringList drivesFromMask(quint32 driveBits) //driveBits ->unitmask
 {
 		QStringList ret;
-
+#if 1  //a disk with multiple partitions removed
 		char driveName[] = "A:/";
 		driveBits &= 0x3ffffff;
 		while (driveBits) {
@@ -49,8 +63,81 @@ static inline QStringList drivesFromMask(quint32 driveBits)
 			++driveName[0];
 			driveBits >>= 1;
 		}
-
 		return ret;
+#else
+		char i = 0;
+		for (; i<26; ++i) {
+			if (driveBits & 0x1)
+				break;
+			driveBits >>= 1;
+		}
+		QChar c(i + 'A');
+		return ret << QString(c) + ":";
+#endif
+}
+
+void static UpdateDevice(PDEV_BROADCAST_DEVICEINTERFACE pDevInf, WPARAM wParam)
+{
+/*
+	// dbcc_name:
+	// \\?\USB#Vid_04e8&Pid_503b#0002F9A9828E0F06#{a5dcbf10-6530-11d2-901f-00c04fb951ed}
+	// convert to
+	// USB\Vid_04e8&Pid_503b\0002F9A9828E0F06
+	ASSERT(lstrlen(pDevInf->dbcc_name) > 4);
+	wchar_t *szDevId = pDevInf->dbcc_name+4;
+	int idx = szDevId.ReverseFind(_T('#'));
+	ASSERT( -1 != idx );
+	szDevId.Truncate(idx);
+	szDevId.Replace(_T('#'), _T('\\'));
+	szDevId.MakeUpper();
+
+	CString szClass;
+	idx = szDevId.Find(_T('\\'));
+	ASSERT(-1 != idx );
+	szClass = szDevId.Left(idx);
+
+	// if we are adding device, we only need present devices
+	// otherwise, we need all devices
+	DWORD dwFlag = DBT_DEVICEARRIVAL != wParam
+		? DIGCF_ALLCLASSES : (DIGCF_ALLCLASSES | DIGCF_PRESENT);
+	HDEVINFO hDevInfo = SetupDiGetClassDevs(NULL, szClass, NULL, dwFlag);
+	if( INVALID_HANDLE_VALUE == hDevInfo )
+	{
+		zDebug("SetupDiGetClassDevs(): " + _com_error(GetLastError()).ErrorMessage());
+		return;
+	}
+
+	SP_DEVINFO_DATA* pspDevInfoData = (SP_DEVINFO_DATA*)HeapAlloc(GetProcessHeap(), 0, sizeof(SP_DEVINFO_DATA));
+	pspDevInfoData->cbSize = sizeof(SP_DEVINFO_DATA);
+	for(int i=0; SetupDiEnumDeviceInfo(hDevInfo,i,pspDevInfoData); ++i) {
+		DWORD DataT ;
+		DWORD nSize=0 ;
+		TCHAR buf[MAX_PATH];
+		if (!SetupDiGetDeviceInstanceId(hDevInfo, pspDevInfoData, buf, sizeof(buf), &nSize)) {
+			zDebug("SetupDiGetDeviceInstanceId(): " + _com_error(GetLastError()).ErrorMessage());
+			break;
+		}
+
+		if (szDevId == buf) {
+			// device found
+			if (SetupDiGetDeviceRegistryProperty(hDevInfo, pspDevInfoData,
+				SPDRP_FRIENDLYNAME, &DataT, (PBYTE)buf, sizeof(buf), &nSize)) {
+				// do nothing
+			} else if (SetupDiGetDeviceRegistryProperty(hDevInfo, pspDevInfoData,
+				SPDRP_DEVICEDESC, &DataT, (PBYTE)buf, sizeof(buf), &nSize)) {
+				// do nothing
+			} else {
+				lstrcpy(buf, _T("Unknown"));
+			}
+			// update UI
+			break;
+		}
+	}
+
+	if (pspDevInfoData)
+		HeapFree(GetProcessHeap(), 0, pspDevInfoData);
+	SetupDiDestroyDeviceInfoList(hDevInfo);
+*/
 }
 
 /*
@@ -79,6 +166,9 @@ dbch_devicetype:
 	Logical volume. This structure is a DEV_BROADCAST_VOLUME structure.
 dbch_reserved:
 	Reserved; do not use.
+WM_DEVICECHANGE限制:
+1 只有顶层窗体的程序才能收到这个消息
+2 仅仅串口、磁盘发生改变，才对每个程序广播这个消息
 */
 LRESULT CALLBACK dw_internal_proc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -126,47 +216,49 @@ LRESULT CALLBACK dw_internal_proc(HWND hwnd, UINT message, WPARAM wParam, LPARAM
 #else
 				QDeviceWatcherPrivate *watcher = (QDeviceWatcherPrivate *)GetWindowLong(hwnd, GWL_USERDATA);
 #endif
-
-				QDeviceChangeEvent *event = 0;
+				QList<QDeviceChangeEvent *> events;
+				QString action_str("add");
+				QDeviceChangeEvent::Action action = QDeviceChangeEvent::Add;
 				if (wParam == DBT_DEVICEARRIVAL) {
-					foreach (const QString &drive, drives) {
-						if (db_volume->dbcv_flags & DBTF_MEDIA)
-							zDebug("Drive %c: Media has been arrived.", drive.at(0).toAscii());
-						else if (db_volume->dbcv_flags & DBTF_NET)
-							zDebug("Drive %c: Network share has been mounted.", drive.at(0).toAscii());
-						else
-							zDebug("Drive %c: Device has been added.", drive.at(0).toAscii());
-						watcher->emitDeviceAdded(drive);
-						if (!watcher->event_receivers.isEmpty())
-							event = new QDeviceChangeEvent(QDeviceChangeEvent::Add, drive);
-					}
 				} else if (wParam == DBT_DEVICEQUERYREMOVE) {
 				} else if (wParam == DBT_DEVICEQUERYREMOVEFAILED) {
 				} else if (wParam == DBT_DEVICEREMOVEPENDING) {
 				} else if (wParam == DBT_DEVICEREMOVECOMPLETE) {
-					foreach (const QString &drive, drives) {
-						if (db_volume->dbcv_flags & DBTF_MEDIA)
-							zDebug("Drive %c: Media has been removed.", drive.at(0).toAscii());
-						else if (db_volume->dbcv_flags & DBTF_NET)
-							zDebug("Drive %c: Network share has been unmounted.", drive.at(0).toAscii());
-						else
-							zDebug("Drive %c: Device has been removed.", drive.at(0).toAscii());
-						watcher->emitDeviceRemoved(drive);
-						if (!watcher->event_receivers.isEmpty())
-							event = new QDeviceChangeEvent(QDeviceChangeEvent::Remove, drive);
-					}
+					action_str = "remove";
+					action = QDeviceChangeEvent::Remove;
 				}
-				if (event != 0 && !watcher->event_receivers.isEmpty()) {
+				foreach (const QString &drive, drives) {
+					if (db_volume->dbcv_flags & DBTF_MEDIA)
+						zDebug("Drive %c: Media has been removed.", drive.at(0).toAscii());
+					else if (db_volume->dbcv_flags & DBTF_NET)
+						zDebug("Drive %c: Network share has been removed.", drive.at(0).toAscii());
+					else
+						zDebug("Drive %c: Device has been removed.", drive.at(0).toAscii());
+					watcher->emitDeviceAction(drive, action_str);
+					if (!watcher->event_receivers.isEmpty())
+						events.append(new QDeviceChangeEvent(action, drive));
+				}
+				if (!events.isEmpty() && !watcher->event_receivers.isEmpty()) {
 					foreach(QObject* obj, watcher->event_receivers) {
-						QCoreApplication::postEvent(obj, event, Qt::HighEventPriority);
+						foreach(QDeviceChangeEvent* event, events) {
+							QCoreApplication::postEvent(obj, event, Qt::HighEventPriority);
+						}
 					}
 				}
 			} else if (lpdb->dbch_devicetype == DBT_DEVTYP_PORT) {
 				zDebug("DBT_DEVTYP_PORT");
+				PDEV_BROADCAST_PORT pDevPort = (PDEV_BROADCAST_PORT)lpdb;
 			} else if (lpdb->dbch_devicetype == DBT_DEVTYP_DEVICEINTERFACE) {
+				//RegisterDeviceNotification()
 				zDebug("DBT_DEVTYP_DEVICEINTERFACE");
+				PDEV_BROADCAST_DEVICEINTERFACE pDevInf = (PDEV_BROADCAST_DEVICEINTERFACE)lpdb;
+				UpdateDevice(pDevInf, wParam);
 			} else if (lpdb->dbch_devicetype == DBT_DEVTYP_OEM) {
 				zDebug("DBT_DEVTYP_OEM");
+				DEV_BROADCAST_OEM *pDevOem = (DEV_BROADCAST_OEM*)lpdb;
+			} else if (lpdb->dbch_devicetype == DBT_DEVTYP_HANDLE) {
+				zDebug("DBT_DEVTYP_HANDLE");
+				PDEV_BROADCAST_HANDLE pDevHnd = (PDEV_BROADCAST_HANDLE)lpdb;
 			}
 			break;
 		case DBT_DEVICETYPESPECIFIC:
@@ -220,6 +312,21 @@ static inline HWND dw_create_internal_window(const void* userData)
 							 0);					 // windows creation data.
 	if (!hwnd) {
 		qWarning("QDeviceWatcherPrivate: Failed to create internal window: %d", (int)GetLastError());
+#if CONFIG_NOTIFICATION
+	} else {
+		DEV_BROADCAST_DEVICEINTERFACE NotificationFilter ;
+		ZeroMemory(&NotificationFilter, sizeof(NotificationFilter)) ;
+		NotificationFilter.dbcc_size = sizeof(DEV_BROADCAST_DEVICEINTERFACE);
+		NotificationFilter.dbcc_devicetype = DBT_DEVTYP_DEVICEINTERFACE;
+		NotificationFilter.dbcc_classguid = InterfaceClassGuid;
+		//3rd param with DEVICE_NOTIFY_ALL_INTERFACE_CLASSES and dbcc_classguid will be ignored
+		//http://msdn.microsoft.com/en-us/library/windows/desktop/aa363431(v=vs.85).aspx
+		hDevNotify = RegisterDeviceNotification(hwnd, &NotificationFilter, DEVICE_NOTIFY_WINDOW_HANDLE);
+		if (!hDevNotify) {
+			zDebug("");
+		}
+	}
+#else
 	} else if (userData) {
 #ifdef GWLP_USERDATA
 		SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)userData);
@@ -227,7 +334,7 @@ static inline HWND dw_create_internal_window(const void* userData)
 		SetWindowLong(hwnd, GWL_USERDATA, (LONG)userData);
 #endif
 	}
-
+#endif //CONFIG_NOTIFICATION
 	return hwnd;
 }
 
@@ -236,6 +343,9 @@ static inline void dw_destroy_internal_window(HWND hwnd)
 	if (hwnd)
 		DestroyWindow(hwnd);
 
+#if CONFIG_NOTIFICATION
+	UnregisterDeviceNotification(hDevNotify);
+#endif
 	UnregisterClass(reinterpret_cast<const wchar_t *>(className().utf16()), qWinAppInst());
 }
 
